aa1dbffd260811fa36bf208fef6d9ff25a7e135a
TopQualityUA <MU@Macs-Mac-mini.local>
2014-10-15 16:43:51 +0300

    added events on options
    added events on options

--- max1/select/JS/main.js
+++ max1/select/JS/main.js
@@ -14,9 +14,11 @@ function func() {
     selectWrapperDiv.appendChild(selectList);
 
     selectDiv.className = "select";
+    selectDiv.tabIndex = -1;
 
     selectedSpan.className = "selected";
     selectedSpan.innerHTML = array[0];
+    selectedSpan.tabIndex = -1;
 
     arrowSpan.innerHTML = '&#8629';
     arrowSpan.className = 'arrow';
@@ -36,6 +38,7 @@ function func() {
         itemDiv.className = "item";
         itemDiv.innerHTML = array[i];
         itemDiv.setAttribute('value', array[i]);
+        itemDiv.tabIndex = -1;
         option.value = array[i];
         option.text = array[i];
         selectList.appendChild(option);
@@ -46,19 +49,63 @@ function func() {
     selectList.addEventListener("click", function (){console.log("native select click triggered");});
     selectList.addEventListener("keyup", function (){console.log("native select keyup triggered");});
 
-    selectedSpan.addEventListener('keyup', function (){
-        var item1 = selectedSpan.parentNode.childNodes[2];
+    selectedSpan.addEventListener('keydown',function(e){
 
+        var item1 = this.parentNode.childNodes[2];
+        console.log(this);
+        console.log(item1);
+        if (e.keyCode == '38'){
+            console.log("ez1");
+            for (var i = item1.childNodes.length - 1; i >= 0; i--) {
+
+                var temp = item1.childNodes[i];
+                console.log(temp.style.display);
+                if (temp.style.display == 'block') {
+                    setTimeout(temp.focus(), 100);
+                    console.log("focused");
+                    return;
+                }
+            }
+        }
+
+        if (e.keyCode == '40'){
+            console.log("ez2");
+            for (var i = 0; i < item1.childNodes.length; i++) {
+
+                var temp = item1.childNodes[i];
+                console.log(temp.style.display);
+                if (temp.style.display == 'block') {
+                    setTimeout(temp.focus(), 100);
+                    console.log(temp);
+                    console.log("focused");
+                    return;
+                }
+            }
+        }
+    });
+
+    selectedSpan.addEventListener('keyup', function (e){
+
+        var item1 = this.parentNode.childNodes[2];
+        var k=0;
+        var l=0;
+        if (e.keyCode == '38') {return;}
+        if (e.keyCode == '40') { return;}
+
+        //console.log("not returned");
         for (var i = item1.childNodes.length - 1; i >= 0; i--) {
             var temp = item1.childNodes[i];
             if (temp.innerHTML.indexOf(selectedSpan.innerHTML)==0)
             {
+                //console.log(temp.style.display);
                 temp.style.display = 'block';
             }
             else{
                 temp.style.display = 'none';
+                k++;
             }
         }
+
         if (item1.style.display=='none') {
             itemsDiv.style.display = 'block';
 
@@ -69,6 +116,7 @@ function func() {
                 }
             }, 0);
         }
+        if (k==(item1.childNodes.length)) {console.log("a");item1.style.display = 'none';}
 
     });
 
@@ -88,7 +136,7 @@ function func() {
         }
         });
 
-    bindEl(selectedSpan);
+
 
     arrowSpan.addEventListener("click", function (el){
         var itemsDiv1 = selectedSpan.parentNode.childNodes[2];
@@ -98,7 +146,7 @@ function func() {
             itemsDiv1.style.display = 'none';
 
         }
-        else{
+        else {
             itemsDiv1.style.display = 'block';
 
             setTimeout(function () {
@@ -111,6 +159,7 @@ function func() {
     });
 
     bindEl(arrowSpan);
+    bindEl(selectedSpan);
 
     var items = selectedSpan.parentNode.childNodes[2].childNodes;
 
@@ -125,12 +174,40 @@ function func() {
                     return;
                 }
             }
-        }
+        };
+        items[i].onkeydown = function(e){
+            console.log(this);
+            console.log(items);
+            var tempItems = items;
+            for (var i=tempItems.length -1; i>=0; i--)
+            {
+                if (this == tempItems[i]){ break;}
+            }
+            if (e.keyCode == '13') {
+                selectedSpan.innerHTML = e.target.getAttribute('value');
+                for (var i= selectList.options.length; i-->0;) {
+                    if (selectList.options[i].value==e.target.getAttribute('value')) {
+                        selectList.selectedIndex= i;
+                        //selectedSpan.click();
+                        return;
+                    }
+                }
+            }
+            if (e.keyCode == '38') {
+                if (i!=0) tempItems[i-1].focus();
+                else tempItems[tempItems.length-1].focus();
+            }
+            if (e.keyCode == '40') {
+                if (i!=tempItems.length-1) tempItems[i+1].focus();
+                else tempItems[0].focus();
+            }
+        };
+
         bindEl(items[i]);
     }
 
     function bindEl(el){
-        el.addEventListener("click", function (){ selectList.click();});
+        el.addEventListener("click", function (){ if (selectList.focus) selectList.click();});
         el.addEventListener("focus", function (){ if (selectList.focus) selectList.focus();});
         el.addEventListener("blur", function (){ if (selectList.blur) selectList.blur();});
         el.addEventListener("change", function (){ if (selectList.change) selectList.change();});
