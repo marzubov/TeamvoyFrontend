849c66e12eb8b4bbb0ba544aaad71d624211c652
TopQualityUA <m.arzubov@gmail.com>
2014-10-15 22:56:15 +0300

    moved project
    moved project

---
+++ select/js/init.js
@@ -0,0 +1,7 @@
+function init(select){
+    var documentSelectArray = [].slice.call(arguments, 0);
+    console.log(documentSelectArray);
+    for (var i = 0; i < documentSelectArray[0].length; i++) {
+        new createCustomSelect(documentSelectArray[0][i]);
+    };
+}
---
+++ select/js/main.js
@@ -0,0 +1,7 @@
+(function(){
+   this.onload=function(){
+    var selects = document.querySelectorAll('select');
+    console.log(selects);
+    new init(selects);
+}
+})()
