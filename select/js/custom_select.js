9286a41fcec72cac5f18d0b3a1da90a1a5e59ccc
TopQualityUA <MU@Macs-Mac-mini.local>
2014-10-17 17:33:40 +0300

    init,main.js,config,index
    renamed initialize into init
    deleted main.js
    fixed reading config array
    added part of styling from config

--- select/js/custom_select.js
+++ select/js/custom_select.js
@@ -4,62 +4,61 @@ function customSelect(container,options,config){
         multiple: "false",
         native: "false",
         replace: "false",
-        tabIndex: "3"
+        defaultOption: "Please Select Option",
+        tabIndex: 0
     };
-    this.initialization(options,config);
+    this.init(options,config);
     container.appendChild(this.selectFragment);
 }
 
-customSelect.prototype.getCustomSelect = function(){
-    return this.selectFragment;
-}
-
-customSelect.prototype.initialization = function(options,config){
+customSelect.prototype.init = function(options,config){
     this.create(options);
     this.configure(config);
     this.events();
 }
 
 customSelect.prototype.create = function(options){
-   this.selectFragment = document.createElement('div');
+    this.selectFragment = document.createElement('div');
     this.selectFragment.tabIndex = 0;
-   var anchor = document.createDocumentFragment();
-   anchor = document.createElement('span');
-    anchor.tabIndex=-1;
-   anchor.innerHTML = "Please Select Option";
-   this.selectFragment.appendChild(anchor);
-   for (var i = 0; i < options.length; i ++) {
+    var anchor = document.createDocumentFragment();
+
+    anchor = document.createElement('span');
+    anchor.tabIndex=0;
+    //anchor.innerHTML = "Please Select Option";
+    this.selectFragment.appendChild(anchor);
+    for (var i = 0; i < options.length; i ++) {
     anchor = document.createElement('div');
     anchor.innerHTML = options[i];
-    anchor.tabIndex=1;
+    anchor.tabIndex=0;
     anchor.style.display = 'none';
     this.selectFragment.appendChild(anchor);
    }
 }
 
 customSelect.prototype.configure = function(config){
-    var that = this.config;
-    Object.keys(that).forEach(function(key,index){
-            if (config[index]){that[key] = config[index];}
+    var myConfig = this.config;
+    //obtaining new config data
+    Object.keys(myConfig).forEach(function(key,index){
+            if (config[index]){myConfig[key] = config[index];}
     });
+    //applying config data to our custom select
     this.selectFragment.className = 'custom-select';
     this.selectFragment.childNodes[0].className = 'custom-selected';
-    Array.prototype.forEach.call(this.selectFragment.childNodes, function (child) {
+    this.selectFragment.childNodes[0].innerHTML = myConfig.defaultOption;
+    Array.prototype.forEach
+        .call(this.selectFragment.childNodes, function (child) {
+        child.tabIndex = myConfig.tabIndex;
         if (child.tagName == 'SPAN') return;
-
         child.className = 'custom-option';
-
     });
-    if (this.config.style == 'white'){
-        this.selectFragment.style.background = 'white';
-    }
 }
 
 customSelect.prototype.events = function(){
-
     var that = this;
+    //click event on our select
     this.selectFragment.addEventListener('click',function(e){
-        Array.prototype.forEach.call(e.target.childNodes, function(child) {
+        Array.prototype.forEach
+            .call(e.target.childNodes, function(child) {
         if((!child.style)||(!child.style.display)||(child.tagName=='SPAN')) return;
         if(child.style.display == 'none'){child.style.display = 'block';}
             else{child.style.display = 'none';}
@@ -67,48 +66,48 @@ customSelect.prototype.events = function(){
         e.preventDefault();
     });
 
-    this.selectFragment.addEventListener('blur',function(){
-        console.log('blur');
-    });
-
-    Array.prototype.forEach.call(this.selectFragment.childNodes, function(child) {
+    Array.prototype.forEach
+        .call(this.selectFragment.childNodes, function(child) {
+        //click events on options and one for span
         child.addEventListener('click',function(){
             if(child.tagName != 'SPAN') this.parentNode.childNodes[0].innerHTML = this.innerHTML;
             this.parentNode.click();
-        })
+        });
+        //key events for options and span
         child.addEventListener('keydown',function(e){
-            console.log(this);
            switch (e.keyCode){
-                case 13:
+                case 13://enter
                     this.click();
                     break;
-                case 27:
+                case 27://esc
                    this.click();
                    break;
-                case 38:
+                case 38://up
                     that.getPreviousOption(e.target).focus();
                     break;
-                case 40:
+                case 40://down
                     that.getNextOption(e.target).focus();
                     break;
                 default:
                     this.parentNode.focus();
                     break;
            }
-            e.preventDefault();
-            return;
+           e.preventDefault();
+           return;
         });
     });
 }
 
 customSelect.prototype.getNextOption = function(option){
-    var index = Array.prototype.indexOf.call(option.parentNode.childNodes, option);
+    var index = Array.prototype.indexOf
+        .call(option.parentNode.childNodes, option);
     if (index==option.parentNode.childNodes.length-1) return option.parentNode.childNodes[0];
     return option.parentNode.childNodes[index+1];
 }
 
 customSelect.prototype.getPreviousOption = function(option){
-    var index = Array.prototype.indexOf.call(option.parentNode.childNodes, option);
+    var index = Array.prototype.indexOf
+        .call(option.parentNode.childNodes, option);
     if (index==0) return option.parentNode.childNodes[option.parentNode.childNodes.length-1];
     return option.parentNode.childNodes[index-1];
 }
