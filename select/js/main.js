a4a10dc916765341168a8b9dd08470fb65eddb16
Tanya <tani4ka26@gmail.com>
2014-10-21 14:29:31 +0300

    delete queue, add classes ,rename variables

--- select/js/main.js
+++ select/js/main.js
@@ -1,9 +1,11 @@
-CustomSelect.createSelectByData(document.getElementById("first"), [1,2,3,4], {defaultOption: "data from array"});
-CustomSelect.createSelectByData(document.getElementById("first"), "file1", {defaultOption: "data from file"});
-CustomSelect.createSelectByData(document.getElementById("first"), "file1", {defaultOption: "data from file"});
-CustomSelect.createSelectByData(document.getElementById("first"), [1,2,3,4], {defaultOption: "data from array"});
-CustomSelect.createSelectByData(document.getElementById("first"), [5,6,7,8], {defaultOption: "data from array"});
-CustomSelect.createSelectByData(document.getElementById("first"), "file1", {defaultOption: "data from file"});
-CustomSelect.createSelectByData(document.getElementById("first"), "file2", {defaultOption: "data from file"});
-CustomSelect.createSelectByData(document.getElementById("first"), [5,5,5,5], {defaultOption: "data from array"});
-CustomSelect.createSelectByData(document.getElementById("first"), "file2", {defaultOption: "data from file"});
+var containerForSelect = document.getElementById("first");
+
+CustomSelect.createSelectByData(containerForSelect, [1,2,3,4], {defaultOption: "data from array"});
+CustomSelect.createSelectByData(containerForSelect, "file1", {defaultOption: "data from file"});
+CustomSelect.createSelectByData(containerForSelect, "file1", {defaultOption: "data from file"});
+CustomSelect.createSelectByData(containerForSelect, [1,2,3,4], {defaultOption: "data from array"});
+CustomSelect.createSelectByData(containerForSelect, [5,6,7,8], {defaultOption: "data from array"});
+CustomSelect.createSelectByData(containerForSelect, "file1", {defaultOption: "data from file"});
+CustomSelect.createSelectByData(containerForSelect, "file2", {defaultOption: "data from file"});
+CustomSelect.createSelectByData(containerForSelect, [5,5,5,5], {defaultOption: "data from array"});
+CustomSelect.createSelectByData(containerForSelect, "file2", {defaultOption: "data from file"});
