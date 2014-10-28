ab5989d23e557a306579ed163b385da8c85487bb
mkozak0108 <mkozak0108@gmail.com>
2014-10-28 17:48:43 +0200

    Godless event machine.

--- javascript/select/js/event_machine.js
+++ javascript/select/js/event_machine.js
@@ -1,36 +1,48 @@
 (function(window,document){
     var EventMachine = window.EventMachine = function(){
-        this.funcArray=[];
+        var funcArray={};
 
-        this.on = function(eventArray,callback){
-            var that=this;
-            eventArray=eventArray.split(' ');
-            eventArray.forEach(function(event) {
-                that.funcArray.push({event: event, callback: callback});
+        /**
+         * Set new event handler
+         * @param events {String} - event names separated by space;
+         * @param eventHandler {Function} - function that will be called when event triggers;
+         * @returns {window.EventMachine}
+         */
+        this.on = function(events,eventHandler){
+            events.split(/\s+/)
+                  .forEach(function(event) {
+                    !funcArray[event] ? funcArray[event]=[]:0;
+                    funcArray[event].push(eventHandler);
             });
             return this;
         };
 
-        this.off = function(eventArray,callback){
-            var that=this;
-            eventArray=eventArray.split(' ');
-            eventArray.forEach(function(event){
-                that.funcArray=that.funcArray.filter(function(el){
-                    return !(el.event == event && el.callback.toString() == callback.toString());
+        /**
+         * Remove event handler
+         * @param events {String} - event names separated by space;
+         * @param eventHandler {Function} - function that will be called when event triggers;
+         * @returns {window.EventMachine}
+         */
+        this.off = function(events,eventHandler){
+            events.split(/\s+/)
+                  .forEach(function(event){
+                    if(funcArray[event])
+                funcArray[event]=funcArray[event].filter(function(handler){
+                    return !(handler === eventHandler);
                 });
             });
             return this;
         };
 
-        this.trigger = function(eventArray){
-            var that=this;
-            eventArray=eventArray.split(' ');
-            eventArray.forEach(function(event) {
-                that.funcArray.forEach(function (el) {
-                    if (el.event == event) {
-                        el.callback();
-                    }
-                });
+        /**
+         * Call listeners of event
+         * @param events {String} - event names separated by space;
+         * @returns {window.EventMachine}
+         */
+        this.trigger = function(events){
+            events.split(/\s+/)
+                  .forEach(function(event) {
+                    funcArray[event].forEach(function(handler){handler()})
             });
             return this;
         }
