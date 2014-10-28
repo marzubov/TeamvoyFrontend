b421a25187b545175104f64cb6742a05f744db72
mkozak0108 <mkozak0108@gmail.com>
2014-10-28 11:57:54 +0200

    Finished event machine in custom select.

--- javascript/select/js/event_machine.js
+++ javascript/select/js/event_machine.js
@@ -1,8 +1,26 @@
 (function(window,document){
     var EventMachine = window.EventMachine = function(){
         this.funcArray=[];
+
         this.on = function(eventName,callback){
             this.funcArray.push({event: eventName, callback: callback});
+            return this;
+        };
+
+        this.off = function(eventName,callback){
+            this.funcArray=this.funcArray.filter(function(el){
+                return el.event != eventName && el.callback != callback;
+            });
+            return this;
+        };
+
+        this.trigger = function(eventName){
+            this.funcArray.forEach(function(el){
+                if(el.event == eventName){
+                    el.callback();
+                }
+            });
+            return this;
         }
     }
 })(window,document);
