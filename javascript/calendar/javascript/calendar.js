909c4672598eb04071bae8c8c019191c42611b27
TopQualityUA <MU@Macs-Mac-mini.local>
2014-10-29 16:47:25 +0200

    added standard calendar generation
    added standard calendar generation and its customizing in other file
    customize_calendar.js

--- javascript/calendar/javascript/calendar.js
+++ javascript/calendar/javascript/calendar.js
@@ -7,6 +7,226 @@
      * @constructor
      */
 
-    window.Calendar = function (container, properties) {};
+    window.Calendar = function (container, properties) {
+        var that = this,
+            model = {},
+            config = {
+                year: (new Date()).getFullYear(),
+                month: (new Date()).getMonth() + 1,
+                firstDayOfWeek: 'sunday',
+                locale: 'en',
+                dayEvents: [
+                    {
+                        message: 'Current day',
+                        date: new Date()
+                    }
+                ]
+            };
+        Calendar.localizationCache = {};
+        this.container = container;
+        this.rootElement = {};
+        init();
+
+        /**
+         * Show current day in calendar
+         */
+        this.showToday = function () {
+            var today = new Date();
+            config.month = today.getMonth() + 1;
+            config.year = today.getFullYear();
+            render();
+            return today;
+        };
+
+        /**
+         * Generating calendar
+         */
+        function generateCalendar() {
+            var dataMonth = Calendar.localizationCache[config.locale],
+                i, date = new Date(config.year, config.month - 1), month, monthPrefix,
+                lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(),
+                firstDayWeek = new Date(date.getFullYear(), date.getMonth(), 1).getDay(),
+                myMonth = ["january", "february", "march", "april", "may", "june", "july", "august",
+                    "september", "october", "november", "december"],
+                myDays = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
+                indexOfStartDay, days = [];
+
+            indexOfStartDay = myDays.indexOf(config.firstDayOfWeek);
+            model.chosenMonth = dataMonth.month[myMonth[config.month - 1]];
+
+            myDays.forEach(function (el, i) {
+                days[i] = dataMonth.daysOfWeek[el];
+            });
+
+            model.arrayOfDays = [];
+            days = days.slice(indexOfStartDay, 7).concat(days.slice(0, indexOfStartDay));
+            model.arrayOfDays.push(days);
+
+            month = Array.apply(null, {length: lastDay}).map(function (el, i) {
+                return i + 1;
+            });
+            firstDayWeek = (7 - (indexOfStartDay + 1 - firstDayWeek)) % 7;
+            monthPrefix = new Array(firstDayWeek);
+            month = monthPrefix.concat(month);
+            for (i = 0; i < month.length; i += 7) {
+                model.arrayOfDays.push(month.slice(i, i + 7));
+            }
+        }
+
+        /**
+         * Getting day event
+         * @param day
+         * @returns {*}
+         */
+        this.getDayEvent = function (day) {
+            return config.dayEvents[0];
+        };
+
+        /**
+         * rendering table
+         */
+        function renderTable() {
+            that.rootElement.classList.add('calendar');
+            var tableString = '', i = 0, newMonthDay = 1,
+                date = new Date(config.year, config.month - 2),
+                lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate(),
+                firstDayWeek = new Date(date.getFullYear(), date.getMonth() + 1, 1).getDay();
+
+            //make caption
+            tableString += '<caption class="caption"><button class="calendar-button desc"></button><span>'
+                + (model.chosenMonth + ' ' + config.year)
+                + '</span><button class="calendar-button asc"></button></caption>';
+
+            //make body
+            tableString += '<tbody>';
+            var weekNumber = 0;
+            model.arrayOfDays.forEach(function (week) {
+                tableString += '<tr>';
+
+                for (i = 0; i < 7; i++) {
+                    if (week[i]) {
+                        tableString += '<td>' + week[i].toString() + '</td>';
+                    } else if (weekNumber == 1) {//previous month non active days
+                        tableString += '<td class="non-active-day">' + (lastDay - firstDayWeek).toString() + '</td>';
+                        firstDayWeek--;
+                    } else {//next month non active days
+                        tableString += '<td class="non-active-day">' + newMonthDay.toString() + '</td>';
+                        newMonthDay++;
+                    }
+                }
+                tableString += '</tr>';
+                weekNumber++;
+            });
+
+            //adding additional non active week to feet standard calendar length
+            tableString += nextMonthDays(newMonthDay);
+
+            tableString += '</tbody>';
+            that.rootElement.innerHTML = tableString;
+        }
+
+        /**
+         * Next month days for rendering additional week
+         * @param startDay - start day for new week
+         * @returns {string} - days of new week of next month
+         */
+        function nextMonthDays(startDay) {
+            //adding next month week if calendar doesn't  feet standard length
+            var newRow = '';
+            for (var j = 0; j < 7 - model.arrayOfDays.length; j++) {
+                newRow += '<tr>';
+                for (var i = 1; i <= 7; i++) {
+                    newRow += '<td class="non-active-day">' + (i + startDay).toString() + '</td>';
+                }
+                newRow += '</tr>';
+            }
+            return newRow
+        }
+
+        /**
+         * Ajax request to get localization
+         * @returns {XMLHttpRequest}
+         */
+        function getLocalization() {
+            var xhr = new XMLHttpRequest();
+            Calendar.localizationCache[config.locale] = XMLHttpRequest;
+            xhr.open("post", "localization/" + config.locale + ".json", true);
+            xhr.send(null);
+            return xhr;
+        }
+
+        /**
+         * setting events on table
+         */
+        function setEvents() {
+            that.rootElement
+                .addEventListener('click', function (e) {
+                    if (e.target.classList.contains('calendar-button')) {
+                        e.target.classList.contains('asc') ? config.month++ : config.month--;
+                        if (config.month > 12) {
+                            config.year++;
+                            config.month = 1;
+                        }
+                        else if (config.month < 1) {
+                            config.year--;
+                            config.month = 12;
+                        }
+                        that.trigger('onMonthChanged');
+                        render();
+                        return true;
+                    }
+                    else if (e.target != this) {
+                        that.trigger('onDayChanged');
+                        return true;
+                    }
+                    return false;
+                });
+        }
+
+        /**
+         * rendering
+         */
+        function render() {
+            generateCalendar();
+            renderTable();
+        }
+
+        /**
+         * initialize
+         */
+        function init() {
+            that.rootElement = document.createElement('table');
+
+            Object.defineProperty(that, "config", {
+                get: function () {
+                    return config;
+                },
+                set: function (value) {
+                    var propName;
+                    for (propName in value) {
+                        value[propName] = value[propName] ?
+                            value[propName] :
+                            config[propName];
+                    }
+                    if (value['month'] != config['month']) {
+                        that.trigger('onMonthChanged');
+                    }
+                    config.merge(value);
+                    render();
+                }
+            });
+
+            config.merge(properties);
+            if (!Calendar.localizationCache[config.locale]) {
+                getLocalization()
+                    .onload = function () {
+                    Calendar.localizationCache[config.locale] = JSON.parse(this.responseText);
+                    render();
+                    container.appendChild(that.rootElement);
+                };
+            }
+            setEvents();
+        }
+    };
     Calendar.prototype = new EventMachine();
 })(window, document);
---
+++ javascript/calendar/javascript/customize_calendar.js
@@ -0,0 +1,54 @@
+function CustomizeCalendar(calendar){
+    var that = this;
+
+    /**
+     *  Customizing days styles
+     */
+    this.day = function (){
+        var weekNumber = 0
+        Array.prototype.slice.call(calendar.rootElement.rows)
+            .forEach(function (row) {
+                Array.prototype.slice.call(row.cells)
+                    .forEach(function (cell) {
+                        if (weekNumber == 0){
+                            // its just day names, so we are skipping this iteration
+                            return false;
+                        }else {
+
+                            //here customizing days
+                            weekNumber++;
+                            //checking if day isn't from another month
+                            if (!cell.classList.contains('non-active-day')){
+                                cell.classList.add('active-day');
+                            }
+                        }
+                    });
+            });
+    };
+
+    /**
+     *  Customizing days names style
+     */
+    this.dayNames = function (){
+        console.log(calendar.rootElement.rows);
+        Array.prototype.slice.call(calendar.rootElement.rows[0].cells)
+            .forEach(function (dayName) {
+                dayName.classList.add('day-name');
+            });
+    };
+
+    this.weekends = function (){
+
+    };
+
+    this.today = function (){
+
+    };
+
+    this.init = function(){
+        that.day();
+        that.dayNames();
+    };
+
+    that.init();
+}
