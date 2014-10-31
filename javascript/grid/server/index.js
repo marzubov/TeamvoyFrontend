5cc702d275f37b3e3ec5218530048273b78c82f8
Tanya <tani4ka26@gmail.com>
2014-10-31 12:57:03 +0200

    delete unnecessary argument, about tables

--- javascript/grid/server/index.js
+++ javascript/grid/server/index.js
@@ -14,19 +14,15 @@ http.createServer(function (request, response) {
         jf.readFile(file, function (err, obj) {
             if (!((path[1]) && (path[2]))) {
                 response.writeHead(200, { 'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*' });
-                console.log(obj.items.length);
                 data = JSON.stringify(obj.items) + "__obj__" + obj.items.length.toString() + "__obj__" + JSON.stringify(obj);
-                //console.log(data);
                 response.end(data);
             }
             //else {
                 for (var i = +path[1]; i < +path[2]; i++) {
-                    console.log(obj[i]);
                     if (obj.items[i])
                         data.push(obj.items[i]);
                 }
                 ;
-                console.log(data);
                 response.writeHead(200, { 'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*' });
                 response.end(JSON.stringify(data) + "__obj__" + obj.items.length.toString()) + "__obj__" + JSON.stringify(obj);;
             //}
