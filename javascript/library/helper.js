e8801b682da6a03807ed1239e9edc05c687b1ab0
TopQualityUA <MU@Macs-Mac-mini.local>
2014-11-05 18:37:28 +0200

    added generating model with moment js
    added generating model with moment js

--- javascript/library/helper.js
+++ javascript/library/helper.js
@@ -91,13 +91,9 @@ function rowForEach(row, func) {
  * @param attrs
  */
 Element.prototype.setAttributes = function (attrs) {
-  for (var idx in attrs) {
-    if ((idx == 'styles' || idx == 'style') && typeof attrs[idx] == 'object') {
-      for (var prop in attrs[idx]){this.style[prop] = attrs[idx][prop]}
-    } else if (idx == 'html') {
-      this.innerHTML = attrs[idx]
-    } else {
-      this.setAttribute(idx, attrs[idx]);
+  for (var key in attrs) {
+    if (this.hasOwnProperty(key)) {
+      this.setAttribute(key, attrs[key]);
     }
   }
 };
