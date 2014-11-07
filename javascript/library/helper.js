d4cebf156fa79cf2d705e5dfb8edf3b4f26ca158
Tanya <tani4ka26@gmail.com>
2014-11-07 14:56:07 +0200

    move helper and handlebar to the library

--- javascript/library/helper.js
+++ javascript/library/helper.js
@@ -98,3 +98,20 @@ Element.prototype.setAttributes = function (attrs) {
     }
   }
 };
+
+function createCORSRequest(method, url) {
+  "use strict";
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest !== "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
