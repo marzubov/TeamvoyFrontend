20a2fc65adc97ea06c56388cd72d214fefff24a8
mkozak0108 <mkozak0108@gmail.com>
2014-11-10 17:34:22 +0200

    Added search box.

--- design/js/main.js
+++ design/js/main.js
@@ -16,5 +16,17 @@
   window.menu.show();
   window.menu.on('hide',function(){
     this.show();
-  })
+  });
+  window.menu.on('change',function(){
+    window.location.replace('index.html#' + window.menu.value);
+  });
+
+   var  dataUsers = [];
+  for(var i=0;i<10;i++){
+    dataUsers.push({
+      "text": faker.name.findName().toUpperCase()
+    })
+  }
+  window.search = new CustomSelect(document.querySelector('.search'),dataUsers,{title: 'text', value:'text'});
+
 })(document,window);
