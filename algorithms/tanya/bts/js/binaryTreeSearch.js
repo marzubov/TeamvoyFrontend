de5d9d2daa10c042561eadab6016d94e3d151470
Tanya <tani4ka26@gmail.com>
2014-11-11 10:45:59 +0200

    add remove and parent to btw

--- algorithms/tanya/bts/js/binaryTreeSearch.js
+++ algorithms/tanya/bts/js/binaryTreeSearch.js
@@ -44,7 +44,7 @@
     BinaryTree.prototype.contains = function(value, node){
         "use strict";
         if (node === null) return false;
-        if (node.value == value) return true;
+        if (node.value == value) return node;
         else if (value < node.value) return this.contains(value, node.left);
         else return this.contains(value, node.right);
     };
@@ -65,6 +65,55 @@
         if (root.left != null) { this.sort(root.left);}
         console.log(root.value);
         if (root.right != null) { this.sort(root.right);}
-    }
+    };
 
+    BinaryTree.prototype.parent = function (value) {
+      var parent = null, isFound = false,
+        currentNode = this.root;
+      while(!isFound && currentNode){
+        if (value < currentNode.value){
+          parent = currentNode;
+          currentNode = currentNode.left;
+        } else if (value > currentNode.value){
+          parent = currentNode;
+          currentNode = currentNode.right;
+        } else {
+          isFound = true;
+        }
+      }
+      return parent;
+    };
+
+    BinaryTree.prototype.remove = function(value) {
+      var parent = null, foundedNode = null, success;
+      if (this.root === null) { console.log("Tree is empty"); return false; }
+      foundedNode = this.contains(value, this.root);
+      parent = this.parent(foundedNode.value);
+      if (!foundedNode) { console.log("Tree hasn't this value"); return false; }
+      if (foundedNode.left && foundedNode.right) {
+        parent = foundedNode;
+        success = foundedNode.right;
+        while (success.left) {
+          parent = success;
+          success = success.left;
+        }
+        foundedNode.value = success.value;
+        foundedNode = success;
+      }
+      if (!foundedNode.left && !foundedNode.right) {
+        if (parent.right == foundedNode) { parent.right = null;
+        } else { parent.left = null; }
+        return true;
+      }
+      if (!foundedNode.left && foundedNode.right) {
+        if (parent.left == foundedNode) { parent.left = foundedNode.right;
+        } else { parent.right = foundedNode.right; }
+        return true;
+      }
+      if (foundedNode.left && !foundedNode.right) {
+        if (parent.left == foundedNode) { parent.left = foundedNode.left;
+        } else { parent.right = foundedNode.left; }
+        return true;
+      }
+    }
 })(window, document);
