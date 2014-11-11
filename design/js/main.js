2aa200bba36f48c2527a7d9c02775c79e5028ea1
TopQualityUA <MU@Macs-Mac-mini.local>
2014-11-11 10:52:02 +0200

    added design folder
    added design folder

---
+++ design/js/main.js
@@ -0,0 +1,32 @@
+(function(document,window){
+  window.menu = new CustomSelect(document.querySelector('.menu'),[
+    {
+      option: 'NEWS'
+    },
+    {
+      option: 'BLOG'
+    },
+    {
+      option: 'ABOUT'
+    },
+    {
+      option: 'CONTACT'
+    }
+  ],{title: 'option', value:'option'});
+  window.menu.show();
+  window.menu.on('hide',function(){
+    this.show();
+  });
+  window.menu.on('change',function(){
+    window.location.replace('index.html#' + window.menu.value);
+  });
+
+   var  dataUsers = [];
+  for(var i=0;i<10;i++){
+    dataUsers.push({
+      "text": faker.name.findName().toUpperCase()
+    })
+  }
+  window.search = new CustomSelect(document.querySelector('.search'),dataUsers,{title: 'text', value:'text'});
+  window.search.selector.placeholder = 'SEARCH';
+})(document,window);
