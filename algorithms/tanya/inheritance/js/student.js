145fd2d6299854ff6c2f5c93d1c718485261d233
Tanya <tani4ka26@gmail.com>
2014-11-11 09:22:48 +0200

    inheritance 1 and 4 methods

--- algorithms/tanya/inheritance/js/student.js
+++ algorithms/tanya/inheritance/js/student.js
@@ -1,9 +1,15 @@
 (function (global, document) {
   global.Student = function Student(name, age, university) {
-    //Person.call(this, name, age);   //2 method
+    Person.call(this, name, age);   //2 method
+    //for(var prop in Person){    //1 method
+    //  if (Person.hasOwnProperty(prop)) {
+    //    if (!this[prop]) this[prop] = [];
+    //    this[prop] = Person[prop];
+    //  }
+    //}
     this.university = university;
   };
-  Student.prototype = Person;   //3 method
+  //Student.prototype = Person;   //3 method
   Student.prototype.information = function () {
     var information = "Hello! My name is " + this.name + ". I'm " + this.age + " old. I'm studying in "
                     + this.university + ".";
