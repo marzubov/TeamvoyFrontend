0b59c741d451dc6b5f95101ac58ecd87abb5d271
TopQualityUA <m.arzubov@gmail.com>
2014-11-12 21:49:21 +0200

    Add jslint validation date_range_picker.js

--- javascript/calendar/javascript/date_range_picker.js
+++ javascript/calendar/javascript/date_range_picker.js
@@ -1,20 +1,21 @@
 (function (global, document) {
   "use strict";
-  var DateRangePicker = global.DateRangePicker = function (container) {
+  global.DateRangePicker = function (container) {
     EventMachine.call(this);
-    var that = this;
+    var that = this,
+      range = {
+        start: moment([2014, 10, 1]),
+        end: moment([2014, 10, 10])
+      };
     this.firstCalendar = {};
     this.secondCalendar = {};
-    var range = this.range = {
-      start: moment([2014, 10, 1]),
-      end: moment([2014, 10, 10])
-    };
+
 
     this.getRange = function () {
       return range;
     };
 
-    this.selectFirstDays = function(){
+    this.selectFirstDays = function () {
       var momentDate,
         calendarBody = that.firstCalendar.getRoot().querySelector('.calendar-body');
       Array.prototype.slice.call(calendarBody.childNodes)
@@ -22,16 +23,22 @@
           momentDate = moment(day.date).locale('en');
 
           //styling selected days
-          if (momentDate.isAfter(range.start) && momentDate.isBefore(range.end)) day.classList.add('selected');
+          if (momentDate.isAfter(range.start) && momentDate.isBefore(range.end)) {
+            day.classList.add('selected');
+          }
 
           //styling start and end
-          if (momentDate.calendar() == range.start.calendar()) day.classList.add('selected-start');
-          if (momentDate.calendar() == range.end.calendar()) day.classList.add('selected-end');
+          if (momentDate.calendar() === range.start.calendar()) {
+            day.classList.add('selected-start');
+          }
+          if (momentDate.calendar() === range.end.calendar()) {
+            day.classList.add('selected-end');
+          }
         });
       return this;
     };
 
-    this.selectSecondDays = function(){
+    this.selectSecondDays = function () {
       var momentDate,
         calendarBody = that.secondCalendar.getRoot().querySelector('.calendar-body');
       Array.prototype.slice.call(calendarBody.childNodes)
@@ -39,11 +46,17 @@
           momentDate = moment(day.date).locale('en');
 
           //styling selected days
-          if (momentDate.isAfter(range.start) && momentDate.isBefore(range.end)) day.classList.add('selected');
+          if (momentDate.isAfter(range.start) && momentDate.isBefore(range.end)) {
+            day.classList.add('selected');
+          }
 
           //styling start and end
-          if (momentDate.calendar() == range.start.calendar()) day.classList.add('selected-start');
-          if (momentDate.calendar() == range.end.calendar()) day.classList.add('selected-end');
+          if (momentDate.calendar() === range.start.calendar()) {
+            day.classList.add('selected-start');
+          }
+          if (momentDate.calendar() === range.end.calendar()) {
+            day.classList.add('selected-end');
+          }
         });
       return this;
     };
@@ -59,45 +72,55 @@
      * Event handler on first date range picker calendar
      * @param e - event object
      */
-    var firstHandler = function (e) {
+    function firstHandler(e) {
       e.preventDefault();
 
-      if (!e.target.date) return false;
+      if (!e.target.date) {
+        return false;
+      }
 
-      var selectedDate = moment(e.target.date).locale('en');
+      var difference,
+        selectedDate = moment(e.target.date).locale('en');
 
-      if (selectedDate.calendar() == range.end.calendar()) return false;
+      if (selectedDate.calendar() === range.end.calendar()) {
+        return false;
+      }
       if (selectedDate.isBefore(range.start)) {
-        var difference = range.end.diff(range.start);
+        difference = range.end.diff(range.start);
         range.end = selectedDate.clone();
         range.start = range.end.clone().subtract(difference, 'milliseconds');
       }
       range.end = selectedDate.clone();
       that.render();
       that.trigger('rangeChanged', [[range.start.clone()._d, range.end.clone()._d]]);
-    };
+    }
 
     /**
      * Event handler on second date range picker calendar
      * @param e
      */
-    var secondHandler = function (e) {
+    function secondHandler(e) {
       e.preventDefault();
 
-      if (!e.target.date) return false;
+      if (!e.target.date) {
+        return false;
+      }
 
-      var selectedDate = moment(e.target.date).locale('en');
+      var difference,
+        selectedDate = moment(e.target.date).locale('en');
 
-      if (selectedDate.calendar() == range.start.calendar()) return false;
+      if (selectedDate.calendar() === range.start.calendar()) {
+        return false;
+      }
       if (selectedDate.isAfter(range.end)) {
-        var difference = range.end.diff(range.start);
+        difference = range.end.diff(range.start);
         range.start = selectedDate.clone();
         range.end = range.start.clone().add(difference, 'milliseconds');
       }
       range.start = selectedDate.clone();
       that.render();
       that.trigger('rangeChanged', [[range.start.clone()._d, range.end.clone()._d]]);
-    };
+    }
 
     this.configMonthAndYear = function () {
       that.firstCalendar.config = {month: range.start.get('month') + 1};
@@ -108,7 +131,7 @@
 
     function init() {
       range.start.locale('en').format('LLL');
-      range.end.locale('en').format('LLL')
+      range.end.locale('en').format('LLL');
 
       that.firstCalendar = new Calendar(container, {});
       that.secondCalendar = new Calendar(container, {});
@@ -120,23 +143,31 @@
 
       //adding mousedown listener
       that.firstCalendar.getRoot().addEventListener('mousedown', function (e) {
-        if (!e.target.date) return false;
+        if (!e.target.date) {
+          return false;
+        }
         secondHandler(e);
         document.addEventListener('mousemove', secondHandler);
       });
       that.secondCalendar.getRoot().addEventListener('mousedown', function (e) {
-        if (!e.target.date) return false;
+        if (!e.target.date) {
+          return false;
+        }
         firstHandler(e);
         document.addEventListener('mousemove', firstHandler);
       });
 
       //adding mouseup listener
       document.addEventListener('mouseup', function (e) {
-        if (!e.target.classList.contains('calendar-button')) that.configMonthAndYear();
+        if (!e.target.classList.contains('calendar-button')) {
+          that.configMonthAndYear();
+        }
         document.removeEventListener('mousemove', secondHandler);
       });
       document.addEventListener('mouseup', function (e) {
-        if (!e.target.classList.contains('calendar-button')) that.configMonthAndYear();
+        if (!e.target.classList.contains('calendar-button')) {
+          that.configMonthAndYear();
+        }
         document.removeEventListener('mousemove', firstHandler);
       });
 
