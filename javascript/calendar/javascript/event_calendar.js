046de0394241cdf60567a5498ecc8801ecb66f39
TopQualityUA <m.arzubov@gmail.com>
2014-11-12 21:20:02 +0200

    Add jslint validation event calendar

--- javascript/calendar/javascript/event_calendar.js
+++ javascript/calendar/javascript/event_calendar.js
@@ -1,27 +1,26 @@
 (function (global, document) {
   "use strict";
 
-  var EventCalendar = global.EventCalendar = function(container){
+  var EventCalendar = global.EventCalendar = function (container) {
     EventMachine.call(this);
-    //moment.locale('en');
     var that = this;
     this.calendar = new Calendar(container, {});
 
-    function setEvents(){
-      that.calendar.on('daySelected', function (day){
-        that.showEvents.call(that,day);
+    function setEvents() {
+      that.calendar.on('daySelected', function (day) {
+        that.showEvents.call(that, day);
       });
-      document.addEventListener('click', function(e){
-       if ((e.target != that.calendar.getRoot())&&(!that.calendar.getRoot().contains(e.target))){
-        that.calendar.getRoot().querySelector('.events-popup')
-          .classList.remove('active');
-         that.calendar.getRoot().querySelector('.events-popup')
-           .classList.add('non-active');
-       }
+      document.addEventListener('click', function (e) {
+        if ((e.target !== that.calendar.getRoot()) && (!that.calendar.getRoot().contains(e.target))) {
+          that.calendar.getRoot().querySelector('.events-popup')
+            .classList.remove('active');
+          that.calendar.getRoot().querySelector('.events-popup')
+            .classList.add('non-active');
+        }
       });
     }
 
-    function init(){
+    function init() {
       var popup = document.createElement('div');
       popup.classList.add('events-popup', 'non-active');
       that.calendar.getRoot().appendChild(popup);
@@ -35,14 +34,17 @@
 
   };
 
-  EventCalendar.prototype.dayEvents = [{date: moment([2014, 10, 10])._d, name:"First Event"}, {date: moment([2014, 10, 10])._d, name:"First Event"}, {date: moment([2014, 10, 10])._d, name:"First Event"}];
+  EventCalendar.prototype.dayEvents = [{
+    date: moment([2014, 10, 10])._d,
+    name: "First Event"
+  }, {date: moment([2014, 10, 10])._d, name: "First Event"}, {date: moment([2014, 10, 10])._d, name: "First Event"}];
 
   EventCalendar.prototype.getDayEvents = function (date) {
 
     //finds day events with same date in config.dayEvents array,
     //and returns array of found day events
     return this.dayEvents.map(function (dayEvent) {
-      if (dayEvent.date.getTime() == date.getTime()) {
+      if (dayEvent.date.getTime() === date.getTime()) {
         return dayEvent;
       }
     });
@@ -53,15 +55,15 @@
    * @param events
    * @returns {HTMLElement}
    */
-  EventCalendar.prototype.eventsTemplate = function (events){
+  EventCalendar.prototype.eventsTemplate = function (events) {
     var eventsElement = document.createElement('div');
     eventsElement.classList.add('events');
     Array.prototype.slice.call(events)
       .forEach(function (dayEvent) {
-        if (dayEvent){
+        if (dayEvent) {
           //eventsElement.innerHTML+=dayEvent.name;
           var dayEventElement = document.createElement('div');
-          dayEventElement.innerHTML+=dayEvent.name;
+          dayEventElement.innerHTML += dayEvent.name;
           eventsElement.appendChild(dayEventElement);
         }
       });
@@ -75,16 +77,16 @@
    */
   EventCalendar.prototype.renderPopup = function (events, dayElement) {
     var dayElementPos = findPos(dayElement),
-      popup = this.calendar.getRoot().querySelector('.events-popup');
+      popup = this.calendar.getRoot().querySelector('.events-popup'),
+      eventsTemplate = this.eventsTemplate(events);
     popup.style.left = (dayElementPos.left + dayElement.offsetWidth).toString() + 'px';
     popup.style.top = (dayElementPos.top + dayElement.offsetHeight).toString() + 'px';
     while (popup.firstChild) {
       popup.removeChild(popup.firstChild);
     }
-    var eventsTemplate = this.eventsTemplate(events);
     if (eventsTemplate instanceof HTMLElement) {
       popup.appendChild(eventsTemplate);
-    } else if (typeof eventsTemplate == "string") {
+    } else if (typeof eventsTemplate === "string") {
       popup.innerHTML = eventsTemplate;
     } else {
       popup.innerHTML = "No events";
@@ -96,7 +98,7 @@
       events = this.getDayEvents(date);
     Array.prototype.slice.call(this.calendar.getRoot().querySelector('.calendar-body').childNodes)
       .some(function (day) {
-        if (date == day.date) {
+        if (date === day.date) {
           selectedDay = day;
           return true;
         }
