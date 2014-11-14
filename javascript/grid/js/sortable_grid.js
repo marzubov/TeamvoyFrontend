20137ba40ab60345398851294c6081a8e79b96a0
Tanya <tani4ka26@gmail.com>
2014-11-14 16:56:10 +0200

    edit index.html with new changes

--- javascript/grid/js/sortable_grid.js
+++ javascript/grid/js/sortable_grid.js
@@ -346,6 +346,22 @@
       }
     }
 
+    function createCORSRequest(method, url) {
+      var xhr = new XMLHttpRequest();
+      if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+      } else if (typeof XDomainRequest !== "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+      } else {
+        // CORS not supported.
+        xhr = null;
+      }
+      return xhr;
+    }
+
     init.call(this);
     return this;
   };
