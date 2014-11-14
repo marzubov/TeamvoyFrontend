b1d9d0a65d3c0ed9c0f340ffb183843a961733fe
MU <m.arzubov@gmail.com>
2014-11-14 11:14:37 +0200

    Program game of life Added checking neighbours function

--- algorithms/max1/GOL/gol.js
+++ algorithms/max1/GOL/gol.js
@@ -32,4 +32,18 @@
   GOL.prototype.nextGen = function nextGen() {
     console.log('nextGen');
   };
+  GOL.prototype.checkNeighbours = function checkNeighbours(x, y) {
+    var i,
+      neighbours = [[x - 1, y - 1], [x, y - 1], [x + 1, y - 1], [x - 1, y], [x + 1, y], [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]],
+      aliveNeighboursCount = 0;
+    for (i = 0; i < 8; i++) {
+      if (this.universe[neighbours[i][0]]) {
+        if (this.universe[neighbours[i][0]][neighbours[i][1]]) {
+          aliveNeighboursCount = aliveNeighboursCount + 1;
+        }
+      }
+    }
+    console.log(aliveNeighboursCount);
+    return aliveNeighboursCount;
+  }
 })(window, document);
