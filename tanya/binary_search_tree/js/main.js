c1cbf5ce361993218985c1fafc6a2c61003c4254
Tanya <tani4ka26@gmail.com>
2014-11-18 16:22:57 +0200

    avl tree

--- tanya/binary_search_tree/js/main.js
+++ tanya/binary_search_tree/js/main.js
@@ -1,10 +1,7 @@
 (function () {
-    var treeExample = new BinaryTree([6,1,7]);
-    console.log("add 5", treeExample.add(5));
-    console.log("add 8", treeExample.add(8));
-    console.log("add 7", treeExample.add(7));
-    console.log("add 2", treeExample.add(2));
-    console.log("add 10", treeExample.add(10));
+    var treeExample = new BinaryTree([6,1,7,5,8,2,10]);
+    treeExample.root = treeExample.add(7);
+    console.log("tree", treeExample.root);
     console.log("parent 10", treeExample.parent(10));
     console.log("remove 7", treeExample.remove(7));
 
