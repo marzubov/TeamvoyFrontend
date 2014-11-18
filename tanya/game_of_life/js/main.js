6882caed14da9f2e017a8c6c4093e26b5e462b0e
Tanya <tani4ka26@gmail.com>
2014-11-18 11:52:28 +0200

    update game: generate, step, div, set time

--- tanya/game_of_life/js/main.js
+++ tanya/game_of_life/js/main.js
@@ -1,6 +1,6 @@
-function game_toggle(game) {
+function game_toggle(game, force) {
   var interval = game.getInterval();
-  if (interval !== null) {
+  if (force == "stop" ||interval !== null) {
     clearInterval(interval);
     game.setTheInterval(null);
   } else {
@@ -9,9 +9,29 @@ function game_toggle(game) {
   }
 }
 
-function add_listeners(game, example_num) {
-  document.getElementById(example_num).addEventListener("click", function(){
+function add_listeners(game, toggle, step, setIntervalButton, generate) {
+  document.getElementById(toggle).addEventListener("click", function(){
     game_toggle(game);
+  });
+  document.getElementById(step).addEventListener("click", function() {
+    game_toggle(game, "stop");
+    game.step();
+  });
+  document.getElementById(setIntervalButton).addEventListener("click", function(){
+    var time = parseInt(document.getElementById("interval").value);
+    var interval = setInterval(game.step, time);
+    game.setTheInterval(interval);
+  });
+  document.getElementById(generate).addEventListener("click", function() {
+    var example1_cells = [], N = 19, M = 44;
+    for (var i = 0; i < N; i++) {
+      example1_cells.push([]);
+      for (var j = 0; j < M; j++) {
+        example1_cells[i].push(Math.round(Math.random()));
+        console.log(Math.round(Math.random()));
+      }
+    }
+    game.setNewFigure(example1_cells);
   });
 }
 
@@ -38,10 +58,11 @@ var example2_cells = [
   [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
 ];
 
+
 var game2 = new GameOfLife({
-  canvas_id:    "life-example",
+  canvas_id:    "gameField",
   cell_width:   10,
   cell_height:  10,
   init_cells:   example2_cells
 });
-add_listeners(game2, "example");
+add_listeners(game2, "toggle", "step", "setInterval", "generate");
