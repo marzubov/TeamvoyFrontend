4fb05d7e59d96e12563dd05041da8f9d2f429686
TopQualityUA <m.arzubov@gmail.com>
2014-11-22 17:17:42 +0200

    Update 2014-11-22T15:17:30.237Z

--- max2/binary_tree/js/main.js
+++
@@ -1,2 +0,0 @@
-﻿var tree = new BalancedBinaryTree();
-tree.addArray([8,5,2,10,39,20,1,3,99,98]);
--- tanya/inheritance/js/main.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  var student = new Student("Tanya", 21, "LNU of I.Franko");
-  student.information();
-})();
